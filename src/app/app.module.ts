8b8285805b4c2cc70a75343030f71c5c838755a4
Chowdhury <akhlak.chowdhury@cgi.com>
2020-09-22 11:01:47 -0400

    Added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,63 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 
+
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {FormsModule} from '@angular/forms';
+
+
+
 @NgModule({
-  declarations: [
+  declarations: [   
+    LoginComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
+
+  MatToolbarModule,
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatDialogModule,
+  MatTableModule,
+  MatMenuModule,
+  MatIconModule,
+  MatProgressSpinnerModule
   ],
+
+  exports: [
+  
+       MatToolbarModule, 
+       MatButtonModule, 
+       MatCardModule, 
+       MatInputModule, 
+       MatDialogModule, 
+       MatTableModule, 
+       MatMenuModule,
+       MatIconModule,
+       MatProgressSpinnerModule
+       ],
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
