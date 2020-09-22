eef9c502d9327f9465de7e2d147a9bce784504b6
Max-Abramson <maxwell.abramson@cgi.com>
2020-09-22 09:45:49 -0400

    add in all page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedComponent } from './feed/feed.component';
+import { SearchComponent } from './search/search.component';
+import { ChatComponent } from './chat/chat.component';
+import { NewIdeaComponent } from './new-idea/new-idea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    FeedComponent,
+    SearchComponent,
+    ChatComponent,
+    NewIdeaComponent
   ],
   imports: [
     BrowserModule,
